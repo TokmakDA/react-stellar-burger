daaac26d4ffce70cfe94a4df099d27af4ef95eab
Dmitry Tokmak <dimka.listok@gmail.com>
2025-02-06 10:16:34 +0200

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,26 +5,28 @@ import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 export const Header: FC = () => {
   return (
-    <header className={`${styles.header} pt-4 pb-4`}>
-      <nav className={styles.nav}>
-        <ul className={styles.nav__list}>
-          <li className={styles.nav__item}>
-            <LinkItem icon={<BurgerIcon type={'primary'} />} to={'#'}>
-              Конструктор
-            </LinkItem>
-          </li>
-          <li>
-            <LinkItem icon={<ListIcon type='primary' />} to={'#'}>
-              Лента заказов
-            </LinkItem>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <Logo />
-      </div>
-      <div>
-        <LinkItem icon={<ProfileIcon type='primary' />} to={'#'}>
+    <header className={`${styles.header}`}>
+      <div className={`${styles.header__wrapper} py-4 ga-2 px-20`}>
+        <nav className={styles.nav}>
+          <ul className={styles.nav__list}>
+            <li className={styles.nav__item}>
+              <LinkItem icon={<BurgerIcon type={'primary'} />} to={'#'}>
+                Конструктор
+              </LinkItem>
+            </li>
+            <li>
+              <LinkItem icon={<ListIcon type='primary' />} to={'#'}>
+                Лента заказов
+              </LinkItem>
+            </li>
+          </ul>
+        </nav>
+        <Logo className={styles.header__logo} />
+        <LinkItem
+          icon={<ProfileIcon type='primary' />}
+          to={'#'}
+          className={styles.header__actions}
+        >
           Личный кабинет
         </LinkItem>
       </div>
--- src/ui-kit/link-item/link-item.tsx
+++ src/ui-kit/link-item/link-item.tsx
@@ -4,24 +4,26 @@ import styles from './link-item.module.scss'
 import { useHover } from '@/hooks'
 
 interface ILinkItem extends LinkProps {
-  icon: ReactElement
-  hoverType?: 'primary' | 'secondary' | 'error'
+  icon?: ReactElement
   children?: ReactNode
+  className?: string
 }
 
-const LinkItem: FC<ILinkItem> = ({ icon, children, ...props }) => {
+const LinkItem: FC<ILinkItem> = ({ icon, children, className, ...props }) => {
   const { isHovered, onMouseEnter, onMouseLeave } = useHover()
 
   return (
     <Link
-      className={`${styles.link} text text_type_main-medium`}
+      className={`${className} ${styles.link} text text_type_main-default ga-2 px-5 py-4`}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       {...props}
     >
-      {cloneElement(icon, {
-        type: isHovered ? 'primary' : 'secondary',
-      })}
+      {icon &&
+        cloneElement(icon, {
+          type: isHovered ? 'primary' : 'secondary',
+          className: styles.link__icon,
+        })}
       {children}
     </Link>
   )
