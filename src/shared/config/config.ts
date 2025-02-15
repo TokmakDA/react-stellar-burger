c25c0fb2af5df62ec94c00ccd9029eaa5a704b51
Tokmak Dmitry <dimka.listok@gmail.com>
2025-02-15 13:46:02 +0200

    to FSD + Redux RTQ

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  import.meta.env.REACT_APP_API_BASE_URL || 'https://norma.nomoreparties.space/api'
+  import.meta.env.REACT_APP_API_BASE_URL ||
+  'https://norma.nomoreparties.space/api'
